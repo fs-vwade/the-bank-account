b111e33b51c0ecfd0d01b4aa367c695c9f4aed0a
Victor Wade <p2525657888@gmail.com>
2024-09-30 20:50:49 -1000

    import store

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import TransactionHistory from "../features/transactions/TransactionHistory";
 import Transactions from "../features/transactions/Transactions";
+import store from "./store";
 
 import "./app.css";
 
